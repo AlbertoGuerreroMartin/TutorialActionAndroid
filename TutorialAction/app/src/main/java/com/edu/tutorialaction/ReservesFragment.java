62560cceddb3f9d4ba4ea61818bcf296a831295f
Alberto Guerrero Martín <alberto170693@gmail.com>
2015-04-07 03:00:24 +0200

    Minor refactor

--- TutorialAction/app/src/main/java/com/edu/tutorialaction/ReservesFragment.java
+++ TutorialAction/app/src/main/java/com/edu/tutorialaction/ReservesFragment.java
@@ -1,27 +1,20 @@
 package com.edu.tutorialaction;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ListView;
 
 import com.edu.tutorialaction.entity.Reserve;
-import com.edu.tutorialaction.network.AuthModel;
 import com.edu.tutorialaction.network.ReserveModel;
 import com.edu.tutorialaction.network.RxLoaderFragment;
 import com.welbits.izanrodrigo.emptyview.library.EmptyView;
 
 import java.util.List;
-import java.util.Map;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
-import rx.Observer;
 
 /**
  * Created by albertoguerreromartin on 16/03/15.
@@ -44,34 +37,6 @@ public class ReservesFragment extends RxLoaderFragment<List<Reserve>> {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
-//        logoutButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                addSubscription(AuthModel.INSTANCE.logout(new Observer<Map<String, String>>() {
-//                    @Override
-//                    public void onCompleted() {
-//
-//                    }
-//
-//                    @Override
-//                    public void onError(Throwable e) {
-//
-//                    }
-//
-//                    @Override
-//                    public void onNext(Map<String, String> stringStringMap) {
-//                        System.out.println("Logout status: " + stringStringMap.get("status"));
-//                        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
-//                        sharedPreferences.edit().remove("api_key").apply();
-//
-//                        Intent intent = new Intent(getActivity(), LoginActivity.class);
-//                        startActivity(intent);
-//                        getActivity().finish();
-//                    }
-//                }, getActivity().getApplicationContext()));
-//            }
-//        });
 
         this.reservesList.setAdapter(this.reservesAdapter = new ReservesAdapter(getActivity()));
         this.emptyView.retry("Reintentar", new Runnable() {
